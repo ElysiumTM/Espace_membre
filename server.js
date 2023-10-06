e6e653a5b992fdf0f251495030feede7ad86a3f7
Jacques Hampartzoumian <jacques@keewe.eu>
2023-10-06 19:09:37 +0200

    Save changes

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
-import userRoutes from './routes/user';
-import route from './routes/routes.js';
+import route from '../../routes/routes.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import flash from 'connect-flash';
 
 // ==========
 // App initialization
 // ==========
 
 dotenv.config();
-const { APP_HOSTNAME, APP_PORT, NODE_ENV } = process.env;
+const { APP_HOSTNAME, APP_PORT, NODE_ENV, NODE_SESSION_SECRET, MONGO_STRING, MONGO_DB_NAME } = process.env;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -19,21 +21,29 @@ const app = express();
 app.set('view engine', 'pug');
 app.locals.pretty = NODE_ENV !== 'production'; // Indente correctement le HTML envoyé au client (utile en dev, mais inutile en production)
 
-const { MONGO_STRING, MONGO_DB_NAME } = process.env;
-
-try {
-  await mongoose.connect(`${MONGO_STRING}${MONGO_DB_NAME}`)
-  console.log('✅ Connecté à la base MongoDB')
-}
-catch (err) {
-  console.error('Erreur de connexion', err.message)
-}
-
 // ==========
 // App middlewares
 // ==========
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  name: "NODE_USER_session",
+  secret: NODE_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({mongoUrl: `${MONGO_STRING}${MONGO_DB_NAME}`})
+}))
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flash_success = req.flash('success');
+  res.locals.flash_error = req?.flash("error");
+  next();
+});
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
@@ -47,6 +57,14 @@ app.use('/', route);
 // ==========
 // App start
 // ==========
+
+try {
+  await mongoose.connect(`${MONGO_STRING}${MONGO_DB_NAME}`)
+  console.log('✅ Connecté à la base MongoDB')
+}
+catch (err) {
+  console.error('Erreur de connexion', err.message)
+}
 
 app.listen(APP_PORT, () => {
   console.log(`App listening at http://${APP_HOSTNAME}:${APP_PORT}`);
